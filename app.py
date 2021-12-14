3b698d18a1fa24d0a9aacccf0b272347579b0350
xeonqq <xeonqq@gmail.com>
2021-12-14 18:39:33 +0100

    Remove flask_mqtt dependency

--- app.py
+++ app.py
@@ -4,37 +4,18 @@ A small Test application to show how to use Flask-MQTT.
 
 """
 import eventlet
+import paho.mqtt.client as mqtt
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
 
 eventlet.monkey_patch()
 
 app = Flask(__name__)
-# app.config['SECRET'] = 'my secret key'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = '0.0.0.0'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
-# app.config['MQTT_CLEAN_SESSION'] = True
-# app.config['MQTT_USERNAME'] = ''
-# app.config['MQTT_PASSWORD'] = ''
-# app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
-# app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
-# app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
-app.config['MQTT_LAST_WILL_QOS'] = 2
 
-# Parameters for SSL enabled
-# app.config['MQTT_BROKER_PORT'] = 8883
-# app.config['MQTT_TLS_ENABLED'] = True
-# app.config['MQTT_TLS_INSECURE'] = True
-# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
-
-mqtt = Mqtt(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
+client = None
 
 SWITCH_POWER_TOPIC = "cmnd/sonoff-socket/POWER"
 SWITCH_POWER_STATE_TOPIC = "stat/sonoff-socket/POWER"
@@ -47,25 +28,18 @@ def index():
 
 @socketio.on('off')
 def handle_off():
-    mqtt.publish(SWITCH_POWER_TOPIC, 'OFF')
+    client.publish(SWITCH_POWER_TOPIC, 'OFF')
 
 
 @socketio.on('on')
 def handle_on():
-    mqtt.publish(SWITCH_POWER_TOPIC, 'ON')
+    client.publish(SWITCH_POWER_TOPIC, 'ON')
 
 
-@mqtt.on_connect()
-def handle_subscribe(client, userdata, flags, rc):
-    mqtt.subscribe(SWITCH_POWER_STATE_TOPIC)
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(SWITCH_POWER_STATE_TOPIC)
 
 
-@socketio.on('unsubscribe_all')
-def handle_unsubscribe_all():
-    mqtt.unsubscribe_all()
-
-
-@mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
     data = dict(
         topic=message.topic,
@@ -75,11 +49,12 @@ def handle_mqtt_message(client, userdata, message):
     socketio.emit('mqtt_message', data=data)
 
 
-@mqtt.on_log()
-def handle_logging(client, userdata, level, buf):
-    # print(level, buf)
-    pass
-
-
 if __name__ == '__main__':
+    client = mqtt.Client("Mqtt-controller", clean_session=True)
+    client.on_connect = on_connect
+    client.on_message = handle_mqtt_message
+    mqtt_broker = "192.168.0.32"
+    client.connect(mqtt_broker)
+    client.loop_start()
+
     socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)

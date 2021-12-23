c097a993489fde0c8d0bfbc35552482eb0d2de59
xeonqq <xeonqq@gmail.com>
2021-12-23 12:22:39 +0100

    Add download page

--- app.py
+++ app.py
@@ -3,11 +3,16 @@
 A small Test application to show how to use Flask-MQTT.
 
 """
+import json
+import urllib
+
 import eventlet
 import paho.mqtt.client as mqtt
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO
+
+from utils import extract_filename
 
 eventlet.monkey_patch()
 
@@ -26,6 +31,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+
 @socketio.on('off')
 def handle_off():
     client.publish(SWITCH_POWER_TOPIC, 'OFF')
@@ -34,6 +44,19 @@ def handle_off():
 @socketio.on('on')
 def handle_on():
     client.publish(SWITCH_POWER_TOPIC, 'ON')
+
+
+@socketio.on('download')
+def handle_download(json_str):
+    print(json_str)
+    data = json.loads(json_str)
+    filename = extract_filename(data['url'])
+
+    data = dict(
+        msg="Downloaded at: {}".format(filename)
+    )
+    socketio.emit('message', data=data)
+    urllib.request.urlretrieve(data['url'], filename)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -53,7 +76,7 @@ if __name__ == '__main__':
     client = mqtt.Client("Mqtt-controller", clean_session=True)
     client.on_connect = on_connect
     client.on_message = handle_mqtt_message
-    mqtt_broker = '0.0.0.0'  # "192.168.0.32"
+    mqtt_broker = '0.0.0.0'  # "192.168.0.28"
     client.connect(mqtt_broker)
     client.loop_start()
 
---
+++ utils.py
@@ -0,0 +1,10 @@
+from os import path
+
+
+def extract_filename(url):
+    fragment_removed = url.split("#")[0]  # keep to left of first #
+    query_string_removed = fragment_removed.split("?")[0]
+    scheme_removed = query_string_removed.split("://")[-1].split(":")[-1]
+    if scheme_removed.find("/") == -1:
+        return ""
+    return path.basename(scheme_removed)
